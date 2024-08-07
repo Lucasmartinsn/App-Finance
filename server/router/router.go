5a120b5c648e0d6654e17c52d43f7d780c171af4
lucas-martins <lucasmartinsnara@gmail.com>
2024-08-07 15:22:31 -0300

    rotas de categoria

--- server/router/router.go
+++ server/router/router.go
@@ -12,15 +12,24 @@ func ConfigRouter(router *gin.Engine) *gin.Engine {
 	router.Use(gin.Recovery())
 
 	// Criando crupos de rotas
-	main := router.Group("v1")
+	main := router.Group("v1/go-fiance")
 	{
-		user := main.Group("go-fiance")
+		user := main.Group("")
 		{
 			user.GET("buscar-usuarios", handles.GetUsers)
 			user.GET("buscar-usuario/:id", handles.GetUserById)
 			user.POST("usuario", handles.CreateUser)
 			user.PUT("usuario/:id", handles.UpdateUser)
 			user.DELETE("usuario/:id", handles.DeleteUser)
+		}
+
+		category := main.Group("")
+		{
+			category.GET("buscar-category/:id", handles.GetCategoryById)
+			category.GET("buscar-categorys", handles.GetCategoriesByUserId)
+			category.POST("buscar-categorys", handles.GetCategories)
+			category.POST("category", handles.CreateCategory)
+			category.PUT("category", handles.UpdateCategory)
 		}
 	}
 	return router
