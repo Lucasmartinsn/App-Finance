5fa2f56293cca2904e3314a94e0c87fe2cf078be
lucas-martins <lucasmartinsnara@gmail.com>
2024-08-15 15:54:53 -0300

    uo

---
+++ web-application/src/app/login.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Conteiner = styled.div`
+    width: 100%;
+    height: 56vw;
+    display: flex;
+    justify-content: center;
+    border: 1px solid red;
+`;
+export const Container_login = styled.div`
+    width: 400px;
+    height: 400px;
+    border-radius: 10%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: 1px solid gold;
+`;
--- web-application/src/app/page.tsx
+++ web-application/src/app/page.tsx
@@ -1,9 +1,65 @@
-export default function Home() {
+"use client"
+import { useState } from 'react';
+// import { useRouter } from 'next/router';
+import { Conteiner, Container_login} from '@/app/login';
+
+export default function LoginPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  // const router = useRouter();
+
+  const handleSubmit = async () => {
+    // Lógica de autenticación aquí
+    try {
+      const res = await fetch('/api/auth/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (res.ok) {
+        // router.push('/dashboard'); // Redirige a la página de dashboard o a la página deseada
+      } else {
+        const data = await res.json();
+        setError(data.message || 'Error en la autenticación');
+      }
+    } catch (err) {
+      setError('Error de red');
+    }
+  };
+
   return (
-    <main className="">
-      <div className="">
-       pagina principal
-      </div>
-    </main>
+    <Conteiner>
+      <Container_login>
+        <h1>Login</h1>
+        <div className="conteiner-form">
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label>Email:</label>
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+            <div>
+              <label>Password:</label>
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+            <button type="submit">Login</button>
+            {error && <p>{error}</p>}
+          </form>
+        </div>
+      </Container_login>
+    </Conteiner>
   );
 }
